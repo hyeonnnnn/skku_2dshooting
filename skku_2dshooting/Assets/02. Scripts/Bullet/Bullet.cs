d1548ba96033734eab2fd4f7cc6b034abe7c52ec
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2025-11-04 16:11:51 +0900

    feat: 총알 S자 움직임

--- skku_2dshooting/Assets/02. Scripts/Bullet/Bullet.cs
+++ skku_2dshooting/Assets/02. Scripts/Bullet/Bullet.cs
@@ -2,10 +2,62 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float Speed = 0.1f;
+    public float Speed;
+    public float StartSpeed = 0.01f;
+    public float EndSpeed = 0.03f;
+    public float Duration = 0.2f;
+    private float _accelation;
+
+    [Header("총알 움직임")]
+    private float _minX = -1.0f;
+    private float _maxX = 1.0f;
+    private float _moveStep = 0.01f;
+    private float _moveDistance = 0f;
+    private bool _isMoveLeft = true;
+
+    private void Start()
+    {
+        Speed = StartSpeed;
+        _accelation = (EndSpeed - StartSpeed) / Duration;
+    }
 
     private void Update()
     {
+        MoveBullet();
+        MoveXAxis();
+    }
+
+    private void MoveBullet()
+    {
+        Speed += _accelation * Time.deltaTime;
+        Speed = Mathf.Min(Speed, EndSpeed);
         transform.Translate(Vector3.up * Speed);
+    }
+
+    private void MoveXAxis()
+    {
+        Vector2 bulletPosition = transform.localPosition;
+
+        if (_isMoveLeft)
+        {
+            bulletPosition.x -= _moveStep;
+            _moveDistance -= _moveStep;
+        }
+        else
+        {
+            bulletPosition.x += _moveStep;
+            _moveDistance += _moveStep;
+        }
+
+        transform.localPosition = bulletPosition;
+
+        if (_moveDistance < _minX)
+        {
+            _isMoveLeft = false;
+        }
+        else if (_moveDistance > _maxX)
+        {
+            _isMoveLeft = true;
+        }
     }
 }
--- skku_2dshooting/Assets/02. Scripts/Player/PlayerFire.cs
+++ skku_2dshooting/Assets/02. Scripts/Player/PlayerFire.cs
@@ -1,19 +1,78 @@
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEditor.PlayerSettings;
 
 public class PlayerFire : MonoBehaviour
 {
     [Header("총알 프리팹")]
     public GameObject BulletPrefab;
+    public GameObject SubBulletPrefab;
 
     [Header("총구")]
-    public Transform FirePosition;
+    public Transform LeftFirePosition;
+    public Transform RightFirePosition;
+    public Transform LeftSubFirePosition;
+    public Transform RightSubFirePosition;
+
+    [Header("쿨타임")]
+    private float _cooltimer = 0f;
+    private float _coolTime = 0.2f;
+    
+    private bool _isAutoAttackMode = true;
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        TryFireBullet();
+        ChangeAttackMode();
+    }
+
+    private void TryFireBullet()
+    {
+        _cooltimer += Time.deltaTime;
+        if (_coolTime < 0) return; // 조기 리턴
+
+        if (_cooltimer > _coolTime)
         {
-            GameObject bullet = Instantiate(BulletPrefab);
-            bullet.transform.position = FirePosition.transform.position;
+            if (_isAutoAttackMode == true)
+            {
+                FireBullet();
+                _cooltimer = 0f;
+            }
+            else if (_isAutoAttackMode == false)
+            {
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    FireBullet();
+                    _cooltimer = 0f;
+                }
+            }
+        }
+    }
+
+    private void FireBullet()
+    {
+        InstantiateBullet(BulletPrefab, LeftFirePosition);
+        InstantiateBullet(BulletPrefab, RightFirePosition);
+        InstantiateBullet(SubBulletPrefab, LeftSubFirePosition);
+        InstantiateBullet(SubBulletPrefab, RightSubFirePosition);
+    }
+
+    private void InstantiateBullet(GameObject prefab, Transform firePosition)
+    {
+        GameObject bullet = Instantiate(prefab, firePosition.position, Quaternion.identity);
+    }
+
+    private void ChangeAttackMode()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _isAutoAttackMode = true;
+            Debug.Log("자동 공격 모드");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _isAutoAttackMode = false;
+            Debug.Log("수동 공격 모드");
         }
     }
 }
