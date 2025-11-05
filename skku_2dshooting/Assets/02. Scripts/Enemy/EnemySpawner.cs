d5d43bce437f903f9fd2aba1ad7d6f3896aa031e
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2025-11-05 14:59:17 +0900

    feat: 스폰 시간을 1~3초 랜덤으로

--- skku_2dshooting/Assets/02. Scripts/Enemy/EnemySpawner.cs
+++ skku_2dshooting/Assets/02. Scripts/Enemy/EnemySpawner.cs
@@ -3,31 +3,33 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _enemyPrefab;
-    [SerializeField] private float _cooltime = 2f;
+    [SerializeField] private float _spawnInterval = 2f;
+
     private float _timer = 0f;
     
     private void Start()
     {
-        float randomNumber = UnityEngine.Random.Range(1f, 2f);
-        _cooltime = randomNumber;
+        ResetSpawnInterval();
     }
 
     private void Update()
     {
-        HandleSpawnTimer();
-    }
-
-    private void HandleSpawnTimer()
-    {
         _timer += Time.deltaTime;
-        if(_timer < _cooltime) return;
+        if (_timer < _spawnInterval) return;
 
         SpawnEnemy();
+        ResetSpawnInterval();
         _timer = 0f;
     }
 
     private void SpawnEnemy()
     {
         GameObject enemy = Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+    }
+
+    private void ResetSpawnInterval()
+    {
+        float randomNumber = UnityEngine.Random.Range(1f, 3f);
+        _spawnInterval = randomNumber;
     }
 }
