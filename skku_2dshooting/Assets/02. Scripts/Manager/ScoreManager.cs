f78d8f8afcb5fc6a3db2561f5f9a194fde32b90c
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2025-11-12 11:03:26 +0900

    refactor: Score을 천 단위 콤마 형식으로 변경

--- skku_2dshooting/Assets/02. Scripts/Manager/ScoreManager.cs
+++ skku_2dshooting/Assets/02. Scripts/Manager/ScoreManager.cs
@@ -4,9 +4,9 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private Text _currentScoreTextUI;
-    private int _cuurentScore = 0;
+    private int _currentScore = 0;
 
-    public int CurrentScore => _cuurentScore;
+    public int CurrentScore => _currentScore;
 
     private void Start()
     {
@@ -17,12 +17,13 @@ public class ScoreManager : MonoBehaviour
     {
         if (score <= 0) return;
 
-        _cuurentScore += score;
+        _currentScore += score;
         Refresh();
     }
 
     private void Refresh()
     {
-        _currentScoreTextUI.text = $"현재 점수: {_cuurentScore}";
+        string formattedScore = string.Format("{0:#,0}", _currentScore);
+        _currentScoreTextUI.text = $"현재 점수: {formattedScore}";
     }
 }
