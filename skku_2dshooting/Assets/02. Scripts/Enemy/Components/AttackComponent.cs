5a1975b3de0ddf0c440ccdc7b2e6a66aa42a2a1a
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2025-11-12 09:38:54 +0900

    feat: Enemy가 플레이어 공격 시 이펙트 추가

--- skku_2dshooting/Assets/02. Scripts/Enemy/Components/AttackComponent.cs
+++ skku_2dshooting/Assets/02. Scripts/Enemy/Components/AttackComponent.cs
@@ -5,6 +5,9 @@ public class AttackComponent : MonoBehaviour
     [Header("공격 설정")]
     [SerializeField] private float _damage;
 
+    [Header("데미지 줄 때 이펙트")]
+    [SerializeField] private ParticleSystem _damageEffect;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Attack(collision);
@@ -19,7 +22,7 @@ public class AttackComponent : MonoBehaviour
         if (player == null) return;
 
         player.TakeDamage(_damage);
-
+        Instantiate(_damageEffect, transform.position, Quaternion.identity);
         Destroy(this.gameObject);
     }
 }
